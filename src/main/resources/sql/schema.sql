4bfd76a9303d6c5d03e7b593da2f30379e17ab17
KIYEONG <rldud1237@naver.com>
2025-08-01 16:00:51 +0900

    fix: 데이터베이스 세팅 초기화

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,36 +1,41 @@
-CREATE TABLE products (
-                          id          BIGINT          PRIMARY KEY AUTO_INCREMENT,
-                          name        VARCHAR(255)    NOT NULL UNIQUE,
-                          price       INT             NOT NULL,
-                          image_url   VARCHAR(4000)    NOT NULL
+CREATE TABLE members
+(
+    id                  BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email               VARCHAR(255) NOT NULL UNIQUE,
+    password            VARCHAR(255) NOT NULL,
+    member_role         VARCHAR(255) NOT NULL,
+    nickname            VARCHAR(255),
+    profile_image_url   VARCHAR(255),
+    kakao_access_token  VARCHAR(512)
 );
 
-CREATE TABLE members (
-                         id          BIGINT          PRIMARY KEY AUTO_INCREMENT,
-                         email       VARCHAR(255)    NOT NULL UNIQUE,
-                         password    VARCHAR(255)    NOT NULL,
-                         member_role        VARCHAR(50)     NOT NULL,
-                         nickname VARCHAR(255),
-                         profile_image_url VARCHAR(255),
-                         kakao_access_token  VARCHAR(512)
+CREATE TABLE products
+(
+    id        BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name      VARCHAR(255) NOT NULL,
+    price     INT          NOT NULL,
+    image_url VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE wishes (
-                        id          BIGINT          PRIMARY KEY AUTO_INCREMENT,
-                        member_id   BIGINT          NOT NULL,
-                        product_id  BIGINT          NOT NULL,
-                        quantity    INT             NOT NULL DEFAULT 1,
-                        FOREIGN KEY (member_id) REFERENCES members(id),
-                        FOREIGN KEY (product_id) REFERENCES products(id),
-                        CONSTRAINT UK_MEMBER_PRODUCT UNIQUE (member_id, product_id)
+CREATE TABLE options
+(
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name       VARCHAR(255) NOT NULL,
+    quantity   INT          NOT NULL,
+    product_id BIGINT,
+    version    BIGINT,
+    FOREIGN KEY (product_id) REFERENCES products (id)
 );
 
-CREATE TABLE options (
-                         id          BIGINT          PRIMARY KEY AUTO_INCREMENT,
-                         name        VARCHAR(50)     NOT NULL,
-                         quantity    INT             NOT NULL,
-                         product_id  BIGINT          NOT NULL,
-                         FOREIGN KEY (product_id) REFERENCES products(id)
+CREATE TABLE wishes
+(
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    member_id  BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    quantity   INT    NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES members (id),
+    FOREIGN KEY (product_id) REFERENCES products (id),
+    CONSTRAINT UK_MEMBER_PRODUCT UNIQUE (member_id, product_id)
 );
 
 CREATE TABLE product_orders
@@ -40,7 +45,7 @@ CREATE TABLE product_orders
     option_id       BIGINT       NOT NULL,
     quantity        INT          NOT NULL,
     order_date_time TIMESTAMP    NOT NULL,
-    order_message         TEXT,
+    order_message   TEXT,
     FOREIGN KEY (member_id) REFERENCES members (id),
     FOREIGN KEY (option_id) REFERENCES options (id)
 );
